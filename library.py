cdf45c23a06e54a1903b3e0a797287f76934daee
austinwarren <102580865+austinwarren@users.noreply.github.com>
2023-11-13 04:43:32 -0800

    Update library.py

--- library.py
+++ library.py
@@ -332,3 +332,16 @@ def threshold_results(thresh_list, actuals, predicted):
 
   fancy_df = result_df.style.format(precision=2).set_properties(**properties).set_table_styles([headers])
   return (result_df, fancy_df)
+
+def halving_search(model, grid, x_train, y_train, factor=2, min_resources="exhaust", scoring='roc_auc'):
+    halving_cv = HalvingGridSearchCV(
+      model, grid,
+      scoring=scoring,
+      n_jobs=-1,
+      min_resources=min_resources,
+      factor=factor,
+      cv=5, random_state=1234,
+      refit=True,
+  )
+
+  return halving_cv.fit(x_train, y_train)
